8c9bd77880715acd9c832502da700d1c27ef66a0
Arun Kushwaha <arunkushwahaayan@gmail.com>
2023-07-01 15:32:32 +0530

    first commit

---
+++ axios/js.js
@@ -0,0 +1,160 @@
+// GET REQUEST
+function getTodos() {
+    axios
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res=> showOutput(res))
+    .catch(err=> console.error(err))
+    
+  }
+  
+  // POST REQUEST
+  function addTodo() {
+    axios
+    .post('https://jsonplaceholder.typicode.com/todos?_limit=10'),{
+      title: 'New Todo',
+      completed: true
+    }
+    .then(res=> showOutput(res))
+    .catch(err=> console.error(err))
+  }
+  
+  // PUT/PATCH REQUEST
+  function updateTodo() {
+    axios
+    .patch('https://jsonplaceholder.typicode.com/todos/1',{
+      title: 'updated',
+      completed: false
+    })
+    .then(res=> showOutput(res))
+    .catch(err=> console.error(err))
+  }
+  
+  // DELETE REQUEST
+  function removeTodo() {
+    axios
+    .delete('https://jsonplaceholder.typicode.com/todos/1')
+    .then(res=> showOutput(res))
+    .catch(err=> console.error(err))
+  }
+  
+  // SIMULTANEOUS DATA
+  function getData() {
+    axios
+    .all([
+      axios.get('https://jsonplaceholder.typicode.com/todos'),
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+    ])
+    .then(axios.spread((todos,posts)=> showOutput(posts)))
+    .catch(err=> console.error(err))
+  }
+  
+  // CUSTOM HEADERS
+  function customHeaders() {
+   const config={
+    headers:{
+      'Content-Type':'application/json',
+      Authorization: 'sometoken'
+    }
+   }
+   axios
+    .post('https://jsonplaceholder.typicode.com/todos?_limit=10'),{
+      title: 'New Todo',
+      completed: true
+    }
+    .then(res=> showOutput(res))
+    .catch(err=> console.error(err))
+  }
+  
+  // TRANSFORMING REQUESTS & RESPONSES
+  function transformResponse() {
+    console.log('Transform Response');
+  }
+  
+  // ERROR HANDLING
+  function errorHandling() {
+    axios
+    .get('https://jsonplaceholder.typicode.com/todos?_limit=10')
+    .then(res=> showOutput(res.data))
+    .catch(err=> {
+      if(err.response){
+        console.log(err.response.data);
+        console.log(err.response.status);
+        console.log(err.response.headers);
+        if(err.response.status===404){
+          alert('Error: Page is not found')
+        }else if(err.request){
+          console.log(err.request)
+        }else{
+          console.error(err.message)
+        }
+
+      }
+    })
+  }
+  
+  // CANCEL TOKEN
+  function cancelToken() {
+    console.log('Cancel Token');
+  }
+  
+  // INTERCEPTING REQUESTS & RESPONSES
+  axios.interceptors.request.use(
+    config=>{
+      console.log(
+        `${config.method.toUpperCase()} request sent to  at ${new Date().getTime()}`
+      )
+      return config;
+    }
+    // error =>{
+    //   return Promise.request(error)
+
+    // }
+  )
+  
+  // AXIOS INSTANCES
+  
+  // Show output in browser
+  function showOutput(res) {
+    document.getElementById('res').innerHTML = `
+    <div class="card card-body mb-4">
+      <h5>Status: ${res.status}</h5>
+    </div>
+    <div class="card mt-3">
+      <div class="card-header">
+        Headers
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.headers, null, 2)}</pre>
+      </div>
+    </div>
+    <div class="card mt-3">
+      <div class="card-header">
+        Data
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.data, null, 2)}</pre>
+      </div>
+    </div>
+    <div class="card mt-3">
+      <div class="card-header">
+        Config
+      </div>
+      <div class="card-body">
+        <pre>${JSON.stringify(res.config, null, 2)}</pre>
+      </div>
+    </div>
+  `;
+  }
+  
+  // Event listeners
+  document.getElementById('get').addEventListener('click', getTodos);
+  document.getElementById('post').addEventListener('click', addTodo);
+  document.getElementById('update').addEventListener('click', updateTodo);
+  document.getElementById('delete').addEventListener('click', removeTodo);
+  document.getElementById('sim').addEventListener('click', getData);
+  document.getElementById('headers').addEventListener('click', customHeaders);
+  document
+    .getElementById('transform')
+    .addEventListener('click', transformResponse);
+  document.getElementById('error').addEventListener('click', errorHandling);
+  document.getElementById('cancel').addEventListener('click', cancelToken);
